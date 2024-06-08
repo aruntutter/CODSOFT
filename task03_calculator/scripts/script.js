61254653173a44727290e97cd3e08a766be4a69a
Arun Kumar R <aruntutter19@gmail.com>
2024-06-09 00:08:13 +0530

    calculator logic added

--- task03_calculator/scripts/script.js
+++ task03_calculator/scripts/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+// function to calculate
+const calculate = (btnValue) => {
+  display.focus();
+
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+// Add event listeners to buttons
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
